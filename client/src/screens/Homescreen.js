c3412601e2b2bada5fe25d1d60f7a7d6579dc34d
bandvov <bandvov@gmail.com>
2021-10-24 22:29:15 +0300

    bind front to backend

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
-import { products } from "../data";
+import axios from "axios";
 
 export default function Homescreen() {
-  return (
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/products")
+      .then((res) => {
+        setProducts(res.data.products);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (e) {
+          setError(true);
+        }
+      });
+  }, []);
+
+  if (error) {
+    return <div>Oops something went wrong</div>;
+  }
+
+  return loading ? (
+    "loading"
+  ) : (
     <div className="row center">
       {products.map((product) => {
         return <Product key={product._id} product={product} />;
